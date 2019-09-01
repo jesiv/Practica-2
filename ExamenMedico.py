9a1cfe3972d48cbb1dc0fb01d27b2ed47f48d3e9
jesiv <jeesivazquez@gmail.com>
2019-08-31 19:51:35 -0500

    Clase Examen Medico

---
+++ ExamenMedico.py
@@ -0,0 +1,12 @@
+class ExamenMedico:
+
+    def __init__(self, idExamen, curp, peso, talla, presion, estatus, fecha, hora, idMedico):
+      self.idExamen = idExamen
+      self.curp = curp
+      self.peso = peso
+      self.talla = talla
+      self.presion = presion
+      self.estatus = estatus
+      self.fecha = fecha
+      self.hora = hora
+      self.idMedico =
